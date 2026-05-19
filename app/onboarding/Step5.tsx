23837d132c1bb3a220056f0cca3a117efb7e817b
RAYMOND Olivier <olivier.raymond.17@eigsi.fr>
2026-05-19 14:48:15 +0200

    Update Step5.tsx

--- app/onboarding/Step5.tsx
+++ app/onboarding/Step5.tsx
@@ -1,6 +1,6 @@
 'use client';
 
-import { Button } from '@/components/ui/button';
+import { Button } from '@/components/layout/ui/button';
 
 export default function OnboardingStep5({ data, complete, prev, loading }: any) {
   return (
